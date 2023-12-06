260e21410f9c6e15fc48df487e570dfd30e7f3d9
binkkeizer <150593660+binkkeizer@users.noreply.github.com>
2023-12-06 14:11:46 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const navButton = document.querySelector("header > button:nth-of-type(1)");
-const deHeader = document.querySelector("header");
-
-navButton.onclick = openCloseNav;
-
-function openCloseNav() {
-	deHeader.classList.toggle("open");
-}
